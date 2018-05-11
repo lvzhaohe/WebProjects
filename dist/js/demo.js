0b571085971e90f84027566bdb64a9c93b862006
kx752457605 <752457605@qq.com>
2018-05-11 18:18:21 +0800

    New

---
+++ dist/js/demo.js
@@ -0,0 +1,95 @@
+/**
+ * AdminLTE Demo Menu
+ * ------------------
+ * You should not use this file in production.
+ * This file is for demo purposes only.
+ */
+$(function () {
+    'use strict'
+
+    /**
+     * Get access to plugins
+     */
+
+    $('[data-toggle="control-sidebar"]').controlSidebar()
+    $('[data-toggle="push-menu"]').pushMenu()
+    var $pushMenu = $('[data-toggle="push-menu"]').data('lte.pushmenu')
+    var $controlSidebar = $('[data-toggle="control-sidebar"]').data('lte.controlsidebar')
+    var $layout = $('body').data('lte.layout')
+    $(window).on('load', function() {
+        // Reinitialize variables on load
+        $pushMenu = $('[data-toggle="push-menu"]').data('lte.pushmenu')
+        $controlSidebar = $('[data-toggle="control-sidebar"]').data('lte.controlsidebar')
+        $layout = $('body').data('lte.layout')
+    })
+
+    /**
+     * List of all the available skins
+     *
+     * @type Array
+     */
+
+    /**
+     * Get a prestored setting
+     *
+     * @param String name Name of of the setting
+     * @returns String The value of the setting | null
+     */
+    function get(name) {
+        if (typeof (Storage) !== 'undefined') {
+            return localStorage.getItem(name)
+        } else {
+            window.alert('Please use a modern browser to properly view this template!')
+        }
+    }
+
+    /**
+     * Store a new settings in the browser
+     *
+     * @param String name Name of the setting
+     * @param String val Value of the setting
+     * @returns void
+     */
+    function store(name, val) {
+        if (typeof (Storage) !== 'undefined') {
+            localStorage.setItem(name, val)
+        } else {
+            window.alert('Please use a modern browser to properly view this template!')
+        }
+    }
+
+    /**
+     * Toggles layout classes
+     *
+     * @param String cls the layout class to toggle
+     * @returns void
+     */
+    function changeLayout(cls) {
+        $('body').toggleClass(cls)
+        $layout.fixSidebar()
+        if ($('body').hasClass('fixed') && cls == 'fixed') {
+            $pushMenu.expandOnHover()
+            $layout.activate()
+        }
+        $controlSidebar.fix()
+    }
+
+    /**
+     * Replaces the old skin with the new skin
+     * @param String cls the new skin class
+     * @returns Boolean false to prevent link's default action
+     */
+    // Create the new tab
+    var $tabPane = $('<div />', {
+        'id': 'control-sidebar-theme-demo-options-tab',
+        'class': 'tab-pane active'
+    })
+
+    // Create the tab button
+    var $tabButton = $('<li />', {'class': 'active'})
+        .html('<a href=\'#control-sidebar-theme-demo-options-tab\' data-toggle=\'tab\'>'
+            + '<i class="fa fa-wrench"></i>'
+            + '</a>')
+
+    $('[data-toggle="tooltip"]').tooltip()
+})
